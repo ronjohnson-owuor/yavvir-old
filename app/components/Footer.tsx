0c5481876cb6f744fae19083db3e4f4acbe6b0e8
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2025-01-09 16:09:02 +0300

    finished the home page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,55 +1,60 @@
-import { Link } from '@remix-run/react';
-import React, { useEffect } from 'react'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { Link } from "@remix-run/react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Footer() {
-
-    useEffect(()=>{
+  useEffect(() => {
     AOS.init();
-  },[])
-  // animation on the footer
+  }, []);
   return (
-    <div data-aos="fade-up"  className='mt-32 bg-beige_light h-[250px] overflow-clip w-full grid grid-cols-3 place-items-center
-    '>
+    <div
+      data-aos="fade-up"
+      className="mt-32 bg-beige_light h-[250px] overflow-clip w-full grid grid-cols-3 place-items-center
+    "
+    >
       <div>
-        <img className='w-[40px] rounded-[100vh]' src="../../public/logo.svg" alt="logo" />
+        <img
+          className="w-[40px] rounded-[100vh]"
+          src="../../public/logo.svg"
+          alt="logo"
+        />
         <p>yavvir by zeron Labs</p>
         <p>nairobi,Kenya</p>
-        <p className='underline'>&copy; yavvir, &copy; zeron labs</p>
+        <p className="underline">&copy; yavvir, &copy; zeron labs</p>
       </div>
       <div>
-        <h3 className='font-bold mb-4'>useful links</h3>
+        <h3 className="font-bold mb-4">useful links</h3>
         <ul>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">become a student</Link>
           </li>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">become an educator</Link>
           </li>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">register a child</Link>
           </li>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">contact us</Link>
           </li>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">about us</Link>
           </li>
         </ul>
       </div>
       <div>
-      <ul>
-          <li className='underline italic'>
+        <ul>
+          <li className="underline italic">
             <Link to="">terms and conditions</Link>
           </li>
-          <li className='underline italic'>
+          <li className="underline italic">
             <Link to="">privacy</Link>
           </li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
